dd9ab5e39205e6140767baa4373fcae33c2fb5ca
Katherine Bensley <katherine.bensley.93@gmail>
2024-02-25 18:51:26 -0500

    letters added

---
+++ script.js
@@ -0,0 +1,20 @@
+// Assignment code here
+const capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const lowercaseLetters = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
